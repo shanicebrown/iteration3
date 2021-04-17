cb6aaadecbae4d433485741d233f4798c469cddb
Shanice Brown <s.mariebrown10@gmail.com>
2021-04-17 18:27:20 -0500

    Initial Commit

---
+++ Shared/iteration3App.swift
@@ -0,0 +1,17 @@
+//
+//  iteration3App.swift
+//  Shared
+//
+//  Created by Shanice Brown (WHA) on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct iteration3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
